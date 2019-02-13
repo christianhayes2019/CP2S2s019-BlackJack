cbbc592823502cd807dfbb05d31d84dce21f6e34
Evania N <evania.nogueras.2021@student.lhschools.org>
2019-02-13 14:15:41 -0500

    Created Players and Deal Method for the Game Class

--- src/org/LickingHeights/Deck.java
+++ src/org/LickingHeights/Deck.java
@@ -1,14 +1,15 @@
 package org.LickingHeights;
 
 
+import java.util.Random;
+
 public class Deck {
 
-    String[] Suits = {"Hearts","Diamonds","Spades","Clubs"};
-    String[] Rank = {"Ace", "2", "3","4","5","6","7","8","9","10","Jack","Queen","King"};
+    String[] Suits = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    String[] Rank = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
 
 
     private Card[] deck;
-
 
 
     public Deck() {
@@ -17,18 +18,18 @@ public class Deck {
 
     }
 
-    private void buildDeck(){
+    private void buildDeck() {
 
-        for(int i = 0; i < deck.length; i++){
+        for (int i = 0; i < deck.length; i++) {
             deck[i] = new Card();
-            deck[i].setSuit(Suits[i/13]);
-            deck[i].setName(Rank[i%13]);
+            deck[i].setSuit(Suits[i / 13]);
+            deck[i].setName(Rank[i % 13]);
 
-            if(i%13<10){
-                deck[i].setValue((i%13)+1);
+            if (i % 13 < 10) {
+                deck[i].setValue((i % 13) + 1);
             }
 
-            if(i%13>=10){
+            if (i % 13 >= 11) {
                 deck[i].setValue(10);
             }
         }
@@ -36,9 +37,10 @@ public class Deck {
 
     }
 
-    public void printDeck(){
-        for(Card card: deck){
+    public void printDeck() {
+        for (Card card : deck) {
             System.out.println(card);
         }
     }
 }
+
