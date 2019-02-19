631202760344a696a80d1db1884e5b8d33b44a89
Unknown <Maximillian.Brown.2021@student.lhschools.org>
2019-02-19 13:22:27 -0500

    Started work on the money

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -3,7 +3,6 @@ package org.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 	// write your code here Hello team! I'm Max
         // Hey everyone, its Evania!
 
