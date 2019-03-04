a91e23325091bcebc4acdb9076ad7d816358c535
Evania N <evania.nogueras.2021@student.lhschools.org>
2019-03-04 13:18:18 -0500

    Updated Code

--- src/org/LickingHeights/BlackJack.java
+++ src/org/LickingHeights/BlackJack.java
@@ -11,21 +11,68 @@ public class BlackJack {
         deck = new Deck();
         players = new ArrayList<>();
         init(4);
+
+        //todo open a loop for the game
+
+        //todo turn loop
+        //todo switch who the current player is
+
+        //todo give the current player option what they want to do for their turn
+
+        //true would have to switch so that game can end.
+        do {
+            //method for dealing cards to every player
+
+
+            for (Player player : players) {
+                System.out.println("money: " + player.getMoney() + " bet: "+ player.getBet());
+                player.bet(10);
+                System.out.println("money: " + player.getMoney() + " bet: "+ player.getBet());
+                player.hitMe(deck.dealCard());
+                player.hitMe(deck.dealCard());
+
+                System.out.println(player.getName());
+                player.getHand().printHand();
+
+                player.hitMe(deck.dealCard());
+                player.bet(30);
+                System.out.println(player.getName());
+                player.getHand().printHand();
+                System.out.println("money: " + player.getMoney() + " bet: "+ player.getBet());
+
+            }
+
+
+
+
+
+        } while(false);
+
+
+
+        //printPlayers();
+
+
+
+
+
+
+
         //displayDeck();
 
-        for(int i=0; i<53;i++) {
+        /*for(int i=0; i<53;i++) {
             displayDeck();
             System.out.println( "\n\n"+   deck.dealCard() );
-        }
+        }*/
 
     }
 
     private void init (int numOfPlayers){
         createPlayers(numOfPlayers);
 
-        for(int i=0; i< 100; i++){
+        /*for(int i=0; i< 100; i++){
             dealCard();
-        }
+        }*/
 
         //placeBet();
        // printPlayers();
